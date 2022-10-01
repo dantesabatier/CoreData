d4e1c23d9e223cba95b30deb2ebe3136fb187d57
Dante <dantesabatier@me.com>
2022-09-30 20:32:30 -0500

    Minor changes

--- src/CoreDataConstantsInternal.php
+++ src/CoreDataConstantsInternal.php
@@ -12,4 +12,4 @@ const SS_COREDATA_DEBUG_XML_STORE = false;
 /** @var bool Temporarily used to bypass SQL Store foreign key checks, but we need to implement a save plan. */
 const SS_COREDATA_DISABLE_FOREIGN_KEY_CHECKS = true;
 /** @var bool There is a bug where the object graph can be truncated when using sort descriptors, this is because in order for {@see SQLFetchRequestContext::executeRequestCore()} to be able to add new leaves to the tree it needs to check the objectID column and with sort descriptors enabled this column may be retrieved in a different order. */
-const SS_COREDATA_CAN_SAFELY_USE_SORT_DESCRIPTORS = false;
+const SS_COREDATA_CAN_SAFELY_USE_SORT_DESCRIPTORS = true;
--- src/IncrementalStore.php
+++ src/IncrementalStore.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection PhpUnused */
 
 /**
  * Created by PhpStorm.
