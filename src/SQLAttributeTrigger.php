afad444fe72d0770e68c5621677e8f3d22ef7e98
Dante <dantesabatier@me.com>
2022-12-22 11:12:39 -0600

    Update SQLAttributeTrigger.php

--- src/SQLAttributeTrigger.php
+++ src/SQLAttributeTrigger.php
@@ -2,19 +2,10 @@
 
 namespace Sabatier\CoreData;
 
-use Exception;
-use Sabatier\Foundation\Dictionary;
 use Sabatier\Foundation\ObjectClass;
 
 class SQLAttributeTrigger extends ObjectClass
 {
-    public function __construct(Dictionary $userInfo, string $attributeName, SQLEntity $entity)
-    {
-    }
-
-    /**
-     * @throws Exception
-     */
     public function validate(): bool
     {
         return false;
