04f9a502b7142e3bc60fa6b53f1d6eb4bddc9097
Dante <dantesabatier@me.com>
2022-12-24 08:58:47 -0600

    Update rector.php

--- rector.php
+++ rector.php
@@ -13,13 +13,14 @@ use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Php80\Rector\FunctionLike\MixedTypeRector;
 use Rector\Php80\Rector\FunctionLike\UnionTypesRector;
 use Rector\Php81\Rector\FuncCall\NullToStrictStringFuncCallArgRector;
+use Rector\Php81\Rector\Property\ReadOnlyPropertyRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
 use Rector\TypeDeclaration\Rector\ClassMethod\ReturnNeverTypeRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/src'
+        __DIR__ . "/src"
     ]);
     $rectorConfig->sets([
         LevelSetList::UP_TO_PHP_82,
@@ -27,17 +28,21 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
     $rectorConfig->skip([
         ClassPropertyAssignToConstructorPromotionRector::class => [
-            __DIR__ . '/src/ManagedObject.php',
-            __DIR__ . '/src/PersistentHistoryChangeRequest.php',
-            __DIR__ . '/src/PersistentHistoryChange.php',
+            __DIR__ . "/src/ManagedObject.php",
+            __DIR__ . "/src/PersistentHistoryChangeRequest.php",
+            __DIR__ . "/src/PersistentHistoryChange.php",
         ],
         OptionalParametersAfterRequiredRector::class => [
-            __DIR__ . '/src/SQLConnection.php',
+            __DIR__ . "/src/SQLConnection.php",
         ],
         CombineIfRector::class => [
-            __DIR__ . '/src/SQLGenerator.php',
+            __DIR__ . "/src/SQLGenerator.php",
         ],
         ExplicitBoolCompareRector::class,
+        ReadOnlyPropertyRector::class => [
+            __DIR__ . "src/SQLStoreMigrator.php",
+            __DIR__ . "src/FaultHandler.php",
+        ],
         ReturnNeverTypeRector::class,
         NullToStrictStringFuncCallArgRector::class,
         UnionTypesRector::class,
