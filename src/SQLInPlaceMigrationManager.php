a466a1467569b9970bca70e9e911cecc032fc738
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-02-26 22:03:20 -0600

    Update SQLInPlaceMigrationManager.php

--- src/SQLInPlaceMigrationManager.php
+++ src/SQLInPlaceMigrationManager.php
@@ -11,16 +11,16 @@ class SQLInPlaceMigrationManager extends MigrationManager
     public function migrateStore(URL $sourceURL, PersistentStoreType $sourceType, ?Dictionary $sourceOptions, MappingModel $mappingModel, URL $destinationURL, PersistentStoreType $destinationType, ?Dictionary $destinationOptions): bool
     {
         if (!$this->prepare($sourceURL, $sourceType, $sourceOptions, $mappingModel, $destinationURL, $destinationType, $destinationOptions)) {
-            return parent::migrateStore($sourceURL, $sourceType, $sourceOptions, $mappingModel, $destinationURL, $destinationType, $destinationOptions);
+            return false;
         }
         /** @var PersistentStoreCoordinator $persistentStoreCoordinator */
         $persistentStoreCoordinator = $this->destinationContext->persistentStoreCoordinator;
-        $destinationStore = $persistentStoreCoordinator->persistentStore($sourceURL);
-        if (!$destinationStore instanceof SQLCore) {
+        $store = $persistentStoreCoordinator->persistentStore($sourceURL);
+        if (!$store instanceof SQLCore) {
             return parent::migrateStore($sourceURL, $sourceType, $sourceOptions, $mappingModel, $destinationURL, $destinationType, $destinationOptions);
         }
-        $model = new SQLModel($this->destinationModel, $destinationStore->configurationName);
-        $migrator = new SQLStoreMigrator($destinationStore, $model, $mappingModel);
+        $model = new SQLModel($this->destinationModel, $store->configurationName);
+        $migrator = new SQLStoreMigrator($store, $model, $mappingModel);
         $migrator->perform();
         $ok = parent::migrateStore($sourceURL, $sourceType, $sourceOptions, $mappingModel, $destinationURL, $destinationType, $destinationOptions);
         $migrator->disconnect();
