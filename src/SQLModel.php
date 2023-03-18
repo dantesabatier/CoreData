a0af1a68c397bd5114b1648b5501c33a22c07f0c
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-03-18 11:28:51 -0600

    Update SQLModel.php

--- src/SQLModel.php
+++ src/SQLModel.php
@@ -22,12 +22,24 @@ class SQLModel extends StoreMapping
 
     public function __construct(public readonly ManagedObjectModel $managedObjectModel, public readonly string $configurationName)
     {
-        $this->entitiesByName = $this->managedObjectModel->entitiesByName->mapValues(fn(EntityDescription $entityDescription): SQLEntity => new SQLEntity($this, $entityDescription));
-        $this->entities = $this->entitiesByName->values;
-        foreach ($this->entities as $entity) {
-            $entity->generateInverseRelationshipsAndMore();
-            $entity->doPostModelGenerationCleanup();
-        }
+        unset($this->entitiesByName);
+        unset($this->entities);
+    }
+
+    public function __get(string $name)
+    {
+        $this->$name = match ($name) {
+            "entitiesByName" => $this->managedObjectModel->entitiesByName->mapValues(fn(EntityDescription $entityDescription): SQLEntity => new SQLEntity($this, $entityDescription)),
+            "entities" => (function(): ArrayClass {
+                $entities = $this->entitiesByName->values;
+                foreach ($entities as $entity) {
+                    $entity->generateInverseRelationshipsAndMore();
+                    $entity->doPostModelGenerationCleanup();
+                }
+                return $entities;
+            })(),
+            default => $this->valueForUndefinedKey($name)
+        };
     }
 
     public function entity(string $named): ?SQLEntity
