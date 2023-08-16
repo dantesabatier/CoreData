5e44332bbf55938f1aedc0cc0bf807283108039b
Dante <dante.palacios@holatrabajo.com>
2023-08-16 10:53:26 -0600

    Small changes

--- src/SQLGenerator.php
+++ src/SQLGenerator.php
@@ -401,7 +401,7 @@ class SQLGenerator extends ObjectClass
 
     private function appendJoinDestinationEntity(SQLEntity $destinationEntity, string $destinationPath): void
     {
-        if ($destinationEntity->isRootEntity || ((int)$destinationEntity->rootEntity?->subentities?->count() < 2)) {
+        if ($destinationEntity->isRootEntity || (int)$destinationEntity->rootEntity?->subentities?->count() < 2) {
             return;
         }
         $this->joinClause .= " AND ";
