0264543bd5c42eb1ba4be1edc7926d7f54d35e53
Dante <dante.palacios@holatrabajo.com>
2023-09-01 14:15:50 -0600

    Update ManagedObject.php

--- src/ManagedObject.php
+++ src/ManagedObject.php
@@ -629,7 +629,7 @@ class ManagedObject extends ObjectClass implements FetchRequestResult
                 assert($value instanceof ManagedObject || $value instanceof ManagedObjectID || $value === null, sprintf("invalid argument: %s(%s) expecting \"%s|%s|null\", \"%s\" given", $this->entity->name, $key, ManagedObject::class, ManagedObjectID::class, typeof($value)));
                 $change = $value;
                 $current = $this->primitiveValueForKey($key);
-                if (!$this->isSuppressingKVO && !$this->isSuppressingChangeNotifications && !$this->objectID->isTemporaryID && !$this->entity->isKindOf($property->inverseRelationship->destinationEntity) && !isset($this->reserved[$key]) && $this->isRelationshipForKeyFault($key)) {
+                if (!$this->isSuppressingKVO && !$this->isSuppressingChangeNotifications && !$this->objectID->isTemporaryID && !isset($this->reserved[$key]) && $this->isRelationshipForKeyFault($key)) {
                     $this->reserved[$key] = true;
                     $current = $this->valueForKey($key);
                     //unset($this->reserved[$key]);
