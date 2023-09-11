96a4edce9bf89c9ab64c7785236becde199accbe
Dante <dante.palacios@holatrabajo.com>
2023-09-11 15:38:35 -0600

    Update SQLStatementFormatter.php

--- src/SQLStatementFormatter.php
+++ src/SQLStatementFormatter.php
@@ -25,7 +25,7 @@ class SQLStatementFormatter extends Formatter
             if ($this->style & SQLStatementFormatterStyle::arguments) {
                 $string = sprintf(str_replace(["%", "?"], ["%%", "%s"], $string), ...$object->arguments->map(fn(mixed $e): string => match (typeof($e)) {
                     ManagedObjectID::class => (string)$e->referenceObject,
-                    Date::class, UUID::class, URL::class, "string" => sprintf("'%s'", str_replace("'", "\'", $e)),
+                    Date::class, UUID::class, URL::class, "string" => sprintf("'%s'", str_replace("'", "\'", (string)$e)),
                     default => (function () use ($e): string {
                         if ($e instanceof Value) {
                             $e = $e->value;
