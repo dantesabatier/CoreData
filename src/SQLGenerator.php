840633c5237bd8dd7f793529838687a0834ec62f
Dante <dante.palacios@holatrabajo.com>
2023-10-11 14:27:34 -0600

    Update SQLGenerator.php

--- src/SQLGenerator.php
+++ src/SQLGenerator.php
@@ -1145,7 +1145,7 @@ class SQLGenerator extends ObjectClass
         $descriptors ??= new ArrayClass();
         $raisesForNotApplicableKeys = $this->raisesForNotApplicableKeys;
         $this->raisesForNotApplicableKeys = false;
-        /** @psalm-suppress InvalidArgument */
+        /** @psalm-suppress InvalidArgument, ArgumentTypeCoercion */
         $descriptors->appendContentsOf($this->keyPathExpressionsForFetchRequestSerialization()->union($this->keyPathExpressionsForFetchRequestPredicate())->flatMap(fn(Expression $expression): iterable => $this->relationshipsFromKeyPathExpression($expression)->filter(fn(SQLRelationship $relationship): bool => $relationship instanceof SQLToMany && $relationship->isOrdered)->map(fn(SQLToMany $relationship): SortDescriptor => new SortDescriptor("{$expression->keyPath()}.{$relationship->inverseToOne->foreignOrderKey->columnName}"))));
         if (!$descriptors->isEmpty()) {
             $clauses = new Set($descriptors->map(fn(SortDescriptor $descriptor): string => sprintf("%s %s", $this->buildKeyPathExpression(Expression::expressionForKeyPath($descriptor->key)), $descriptor->ascending ? "ASC" : "DESC"))->filter(fn(string $string): bool => str_contains($string, ".")));
