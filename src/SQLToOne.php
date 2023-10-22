ede3116d0ec9170b61a07dab7e454d926d689f73
Dante <113949719+dantesabatier@users.noreply.github.com>
2023-10-21 20:19:48 -0600

    Update SQLToOne.php

--- src/SQLToOne.php
+++ src/SQLToOne.php
@@ -20,17 +20,9 @@ class SQLToOne extends SQLRelationship
     public function __construct(SQLEntity $entity, RelationshipDescription $relationshipDescription)
     {
         parent::__construct($entity, $relationshipDescription);
-        $isVirtual = false;
-        $relationshipDescription = $this->relationshipDescription;
-        if ($rootEntity = $relationshipDescription->destinationEntity->rootEntity) {
-            $relationshipDescription = clone $relationshipDescription;
-            $relationshipDescription->destinationEntity = $rootEntity;
-            $this->lazyDestinationEntityName = $relationshipDescription->destinationEntity->name;
-            $isVirtual = true;
-        }
         $this->foreignKey = new SQLForeignKey($entity, $relationshipDescription, $this);
-        $this->foreignEntityKey = new SQLForeignEntityKey($entity, $this->relationshipDescription, $this->foreignKey);
+        $this->foreignEntityKey = new SQLForeignEntityKey($entity, $relationshipDescription, $this->foreignKey);
         $this->foreignOrderKey = new SQLForeignOrderKey($entity, $relationshipDescription->inverseRelationship, new SQLForeignKey($entity, $relationshipDescription->inverseRelationship, $this));
-        $this->isVirtual = $isVirtual;
+        $this->isVirtual = false;
     }
 }
