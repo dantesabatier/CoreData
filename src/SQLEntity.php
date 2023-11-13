31ec286190d4983ae54475ff0c9d5bd99a62059a
Dante <dante.palacios@holatrabajo.com>
2023-11-13 14:12:39 -0600

    Update SQLEntity.php

--- src/SQLEntity.php
+++ src/SQLEntity.php
@@ -219,6 +219,7 @@ class SQLEntity extends StoreMapping
             };
             /** @var Dictionary<SQLIndex> $indexes */
             $indexes = new Dictionary([self::entityKeyName => new SQLIndex(new FetchIndexDescription(self::entityKeyName, new ArrayClass([new FetchIndexElementDescription($this->entityKey->propertyDescription)])), $this)]);
+            /** @psalm-suppress PossiblyInvalidArgument */
             $indexes->merge($this->entityDescription->indexes->reduce(new Dictionary(), $updateAccumulatingResult));
             foreach ($this->entityDescription->subentities as $subentity) {
                 /** @psalm-suppress PossiblyInvalidArgument */
