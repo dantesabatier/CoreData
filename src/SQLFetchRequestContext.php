af747d399be5968d7174f3e92c7056fa6e58c349
Dante <dantesabatier@me.com>
2023-11-19 17:46:27 -0600

    Update SQLFetchRequestContext.php

--- src/SQLFetchRequestContext.php
+++ src/SQLFetchRequestContext.php
@@ -146,6 +146,10 @@ class SQLFetchRequestContext extends SQLStoreRequestContext
             $message = sprintf("CoreData: annotation: total execution time: %s for %s element(s)", human_readable_time(absolute_time_get_current() - $time), $this->result->count());
             if ($this->debugLogLevel > 3) {
                 $message .= " $this->result";
+                if ($this->debugLogLevel > 4) {
+                    $execute = $this->connection->execute(new SQLStatement("ANALYZE FORMAT=JSON {$this->fetchStatement->string}", $this->fetchStatement->arguments));
+                    $message .= $execute->fetchColumn();
+                }
             }
             error_log($message);
         }
