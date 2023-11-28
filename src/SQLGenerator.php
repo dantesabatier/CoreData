d441f3c1728ee415742f1b471dff0c676094ac8b
Dante <dante.palacios@holatrabajo.com>
2023-11-28 12:18:04 -0600

    Update SQLGenerator.php

--- src/SQLGenerator.php
+++ src/SQLGenerator.php
@@ -1217,7 +1217,7 @@ class SQLGenerator extends ObjectClass
                 if ($property instanceof SQLPrimaryKey || $property instanceof SQLEntityKey) {
                     $columnNames->append($property->name);
                 } elseif ($property instanceof SQLAttribute) {
-                    if ($insertedObject->changedValuesForCurrentEvent()[$property->name]) {
+                    if ($insertedObject->changedValuesForCurrentEvent()[$property->name] !== null) {
                         $columnNames->append($property->columnName);
                     }
                 } elseif ($property instanceof SQLToOne) {
