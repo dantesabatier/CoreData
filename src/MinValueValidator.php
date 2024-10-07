5a0cc091c63f7f9bcd72ec768649dd0af2cf579e
dantesabatier <dantesabatier@me.com>
2024-10-06 23:11:31 -0600

    Update MinValueValidator.php

--- src/MinValueValidator.php
+++ src/MinValueValidator.php
@@ -2,9 +2,12 @@
 
 namespace Sabatier\CoreData;
 
+use Override;
+
 /** @internal */
 class MinValueValidator extends ValueValidator
 {
+    #[Override]
     public function validate(mixed $object): bool
     {
         return $this->coerce($object) >= $this->value;
