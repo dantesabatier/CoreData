45d5c7f9c8728bfa4884bb64674b896cd7a9dab6
dantesabatier <dantesabatier@me.com>
2024-11-04 14:20:14 -0600

    Update AttributeType.php

--- src/AttributeType.php
+++ src/AttributeType.php
@@ -37,4 +37,7 @@ enum AttributeType: int
     case transformable = 1800;
     /** An attribute that stores a managed object's ID. */
     case objectID = 2000;
+    /** An attribute that derives its value by composing other attributes. Composite attributes support all attribute types except the following: {@see AttributeType::undefinedAttributeType}, {@see AttributeType::objectIDAttributeType} and {@see AttributeType::binaryDataAttributeType} (when {@see AttributeDescription::allowsExternalBinaryDataStorage} is true)
+     */
+    case compositeAttributeType = 2100;
 }
