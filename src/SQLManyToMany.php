e7d9299d0591456f44761c3a141658f235e0a216
dantesabatier <dantesabatier@me.com>
2024-11-17 23:53:35 -0600

    Update SQLManyToMany.php

--- src/SQLManyToMany.php
+++ src/SQLManyToMany.php
@@ -21,58 +21,30 @@ class SQLManyToMany extends SQLRelationship
         /** @noinspection PhpIncompatibleReturnTypeInspection */
         get => $this->inverseRelationship;
     }
-    public readonly string $correlationTableName;
+    public string $correlationTableName {
+        get => $this->associatedValues[__PROPERTY__] ??= new ArrayClass([$this->destinationEntity, $this->inverseRelationship->destinationEntity])->sorted([new SortDescriptor("tableName")])->valueForKey("tableName")->join("");
+    }
     public string $columnName {
-        get => "{$this->name}ID";
+        get => $this->associatedValues[__PROPERTY__] ??= "{$this->name}ID";
     }
     public SQLType $columnSQLType {
         get => SQLType::int;
     }
-    public readonly string $orderColumnName;
+    public string $orderColumnName {
+        get => $this->associatedValues[__PROPERTY__] ??= $this->isReflexive ? $this->columnName : new ArrayClass([$this->columnName, $this->inverseColumnName])->sort(fn(string $e, string $e1): int => ComparisonResult::orderedAscending->value * ($e <=> $e1))[0];
+    }
     public readonly SQLType $orderColumnSQLType;
     public string $inverseColumnName {
         get => $this->inverseManyToMany->columnName;
     }
-    public readonly string $inverseOrderColumnName;
+    public string $inverseOrderColumnName {
+        get => $this->associatedValues[__PROPERTY__] ??= $this->isReflexive ? $this->columnName : new ArrayClass([$this->columnName, $this->inverseColumnName])->sort(fn(string $e, string $e1): int => ComparisonResult::orderedAscending->value * ($e <=> $e1))[1];
+    }
     public bool $isReflexive {
         get => $this->columnName === $this->inverseColumnName;
     }
     public bool $isMaster {
         get => false;
-    }
-
-    public function __construct(SQLEntity $entity, RelationshipDescription $relationshipDescription)
-    {
-        parent::__construct($entity, $relationshipDescription);
-        unset($this->correlationTableName);
-        unset($this->orderColumnName);
-        unset($this->inverseOrderColumnName);
-    }
-
-    public function __get(string $name)
-    {
-        if ($name === "correlationTableName") {
-            $this->$name = new ArrayClass([$this->destinationEntity, $this->inverseRelationship->destinationEntity])->sorted([new SortDescriptor("tableName")])->valueForKey("tableName")->join("");
-            return $this->$name;
-        }
-        if ($name === "orderColumnName") {
-            $this->$name = $this->isReflexive ? $this->columnName : new ArrayClass([$this->columnName, $this->inverseColumnName])->sort(fn(string $e, string $e1): int => ComparisonResult::orderedAscending->value * ($e <=> $e1))[0];
-            return $this->$name;
-        }
-        if ($name === "inverseOrderColumnName") {
-            $this->$name = $this->isReflexive ? $this->columnName : new ArrayClass([$this->columnName, $this->inverseColumnName])->sort(fn(string $e, string $e1): int => ComparisonResult::orderedAscending->value * ($e <=> $e1))[1];
-            return $this->$name;
-        }
-        return $this->valueForUndefinedKey($name);
-    }
-
-    public function __set(string $name, mixed $value): void
-    {
-        if ($name === "correlationTableName" || $name === "orderColumnName" || $name === "inverseOrderColumnName") {
-            $this->$name = $value;
-        } else {
-            $this->setValueForUndefinedKey($value, $name);
-        }
     }
 
     #[Override]
