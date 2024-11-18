8a193c064b9e27da2d61f088eb237f778269ca32
dantesabatier <dantesabatier@me.com>
2024-11-18 03:18:29 -0600

    Update SQLToOne.php

--- src/SQLToOne.php
+++ src/SQLToOne.php
@@ -12,18 +12,16 @@ namespace Sabatier\CoreData;
 /** @internal */
 class SQLToOne extends SQLRelationship
 {
-    public readonly SQLForeignKey $foreignKey;
-    public readonly SQLForeignEntityKey $foreignEntityKey;
-    public readonly SQLForeignOrderKey $foreignOrderKey;
+    private(set) SQLForeignKey $foreignKey {
+        get => $this->foreignKey ??= new SQLForeignKey($this->entity, $this->relationshipDescription, $this);
+    }
+    private(set) SQLForeignEntityKey $foreignEntityKey {
+        get => $this->foreignEntityKey ??= new SQLForeignEntityKey($this->entity, $this->relationshipDescription, $this->foreignKey);
+    }
+    private(set) SQLForeignOrderKey $foreignOrderKey {
+        get => $this->foreignOrderKey ??= new SQLForeignOrderKey($this->entity, $this->relationshipDescription->inverseRelationship, new SQLForeignKey($this->entity, $this->relationshipDescription->inverseRelationship, $this));
+    }
     public bool $isVirtual {
         get => false;
-    }
-
-    public function __construct(SQLEntity $entity, RelationshipDescription $relationshipDescription)
-    {
-        parent::__construct($entity, $relationshipDescription);
-        $this->foreignKey = new SQLForeignKey($entity, $relationshipDescription, $this);
-        $this->foreignEntityKey = new SQLForeignEntityKey($entity, $relationshipDescription, $this->foreignKey);
-        $this->foreignOrderKey = new SQLForeignOrderKey($entity, $relationshipDescription->inverseRelationship, new SQLForeignKey($entity, $relationshipDescription->inverseRelationship, $this));
     }
 }
