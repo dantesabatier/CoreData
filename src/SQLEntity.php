f11e6ad557798e76a79f40a8318e7a8272b019e3
dantesabatier <dantesabatier@me.com>
2024-11-19 15:15:26 -0600

    Update SQLEntity.php

--- src/SQLEntity.php
+++ src/SQLEntity.php
@@ -111,8 +111,19 @@ class SQLEntity extends StoreMapping
     private(set) ArrayClass $columnsToCreate {
         get => $this->columnsToCreate ??= $this->properties->filter(fn(SQLProperty $property): bool => !$property instanceof SQLRelationship && !($property instanceof SQLAttribute && $property->isDerivedAttribute && $property->derivationExpression?->usesKVC));
     }
-    private(set) Dictionary $compositeAttributeNameToSQLProperties {
-        get => $this->compositeAttributeNameToSQLProperties ??= new Dictionary();
+    private(set) Dictionary $compositeAttributeNamesToSQLProperties {
+        get => $this->compositeAttributeNamesToSQLProperties ??= $this->attributes->reduce(new Dictionary(), function (Dictionary $result, SQLAttribute $attribute): Dictionary {
+            if ($attribute->attributeDescription->type === AttributeType::compositeAttributeType) {
+                $compositeAttribute = $attribute->attributeDescription;
+                if ($compositeAttribute instanceof CompositeAttributeDescription) {
+                    $result[$attribute->name] = $compositeAttribute->elements->reduce(new Dictionary(), function (Dictionary $result, AttributeDescription $element): Dictionary {
+                        $result[$element->name] = new SQLAttribute($this, $element);
+                        return $result;
+                    });
+                }
+            }
+            return $result;
+        });
     }
     //TODO: not implemented
     public int $entityID = 0;
