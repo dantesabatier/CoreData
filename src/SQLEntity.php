400e226703eb3e8eb4af6528af35f605b0cb005f
dantesabatier <dantesabatier@me.com>
2024-11-20 12:45:37 -0600

    Update SQLEntity.php

--- src/SQLEntity.php
+++ src/SQLEntity.php
@@ -253,7 +253,9 @@ class SQLEntity extends StoreMapping
 
     private function columnsToFetch(): ArrayClass
     {
-        $columns = $this->properties->filter(fn(SQLProperty $property): bool => $property instanceof SQLAttribute ? ($property->isDerivedAttribute ? !$property->derivationExpression?->usesKVC : !$property->isCompositeAttribute) : !$property instanceof SQLRelationship && !$property instanceof SQLForeignKey && !$property->isTransient);
+        $columns = $this->properties->filter(function (SQLProperty $property): bool {
+            return !($property->isTransient || $property instanceof SQLRelationship || $property instanceof SQLForeignKey) && (!$property instanceof SQLAttribute || (($property->isDerivedAttribute ? !$property->derivationExpression?->usesKVC : !$property->isCompositeAttribute)));
+        });
         $columns->appendContentsOf($this->compositeAttributeNameToSQLAttributes->values->flatMap(fn(Dictionary $dictionary
         ): ArrayClass => $dictionary->values));
         return $columns;
