bf6118a29630e8e4db64bcf82541b2b3bd0a0cb7
dantesabatier <dantesabatier@me.com>
2024-11-23 10:24:54 -0600

    Update SQLStatementFormatter.php

--- src/SQLStatementFormatter.php
+++ src/SQLStatementFormatter.php
@@ -22,35 +22,35 @@ class SQLStatementFormatter extends Formatter
     #[Override]
     public function string(mixed $object): ?string
     {
-        if ($object instanceof SQLStatement) {
-            $string = $object->string;
-            if ($this->style & SQLStatementFormatterStyle::arguments) {
-                $string = sprintf(str_replace(["%", "?"], ["%%", "%s"], $string), ...$object->arguments->map(fn(mixed $e): string => match (typeof($e)) {
-                    ManagedObjectID::class => (string)$e->referenceObject,
-                    Date::class, UUID::class, URL::class => "'$e'",
-                    "string" => mb_check_encoding($e, "UTF-8") ? "'" . addslashes((string) $e) . "'" : "(binary data)",
-                    default => (function () use ($e): string {
-                        if ($e instanceof Value) {
-                            $e = $e->value;
-                        }
-                        $v = human_readable_value($e);
-                        if (is_bool($e) || is_null($e)) {
-                            return strtoupper($v);
-                        }
-                        return $v;
-                    })()
-                })->array);
-            }
-            $style = SQLFormatterStyle::none;
-            if ($this->style & SQLStatementFormatterStyle::highlighted) {
-                $style |= SQLFormatterStyle::highlighted;
-            }
-            if ($this->style & SQLStatementFormatterStyle::prettyPrint) {
-                $style |= SQLFormatterStyle::prettyPrint;
-            }
-            $formatter = new SQLFormatter($style);
-            return $formatter->string($string);
+        if (!$object instanceof SQLStatement) {
+            return null;
         }
-        return null;
+        $string = $object->string;
+        if ($this->style & SQLStatementFormatterStyle::arguments) {
+            $string = sprintf(str_replace(["%", "?"], ["%%", "%s"], $string), ...$object->arguments->map(fn(mixed $e): string => match (typeof($e)) {
+                ManagedObjectID::class => (string)$e->referenceObject,
+                Date::class, UUID::class, URL::class => "'$e'",
+                "string" => mb_check_encoding($e, "UTF-8") ? "'" . addslashes((string) $e) . "'" : "(binary data)",
+                default => (function () use ($e): string {
+                    if ($e instanceof Value) {
+                        $e = $e->value;
+                    }
+                    $v = human_readable_value($e);
+                    if (is_bool($e) || is_null($e)) {
+                        return strtoupper($v);
+                    }
+                    return $v;
+                })()
+            })->array);
+        }
+        $style = SQLFormatterStyle::none;
+        if ($this->style & SQLStatementFormatterStyle::highlighted) {
+            $style |= SQLFormatterStyle::highlighted;
+        }
+        if ($this->style & SQLStatementFormatterStyle::prettyPrint) {
+            $style |= SQLFormatterStyle::prettyPrint;
+        }
+        $formatter = new SQLFormatter($style);
+        return $formatter->string($string);
     }
 }
