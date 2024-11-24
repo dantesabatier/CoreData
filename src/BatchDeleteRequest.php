722094cafc5573e7c57ddff354aa1951a84540b5
dantesabatier <dantesabatier@me.com>
2024-11-24 04:44:55 -0600

    Update BatchDeleteRequest.php

--- src/BatchDeleteRequest.php
+++ src/BatchDeleteRequest.php
@@ -19,11 +19,11 @@ class BatchDeleteRequest extends PersistentStoreRequest
     /** @var BatchDeleteRequestResultType The type of result the request provides when it executes. */
     public BatchDeleteRequestResultType $resultType = BatchDeleteRequestResultType::statusOnly;
 
-    #[Pure]
     /**
      * Creates a request that deletes the results of the specified fetch request.
      * @param FetchRequest $fetchRequest The fetch request that identifies the managed objects to delete.
      */
+    #[Pure]
     public function __construct(public readonly FetchRequest $fetchRequest)
     {
         parent::__construct(PersistentStoreRequestType::batchDeleteRequestType);
