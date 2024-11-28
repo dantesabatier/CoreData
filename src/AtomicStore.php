0fa0048a73146f204704810bc1b77660d5071932
dantesabatier <dantesabatier@me.com>
2024-11-28 09:39:15 -0600

    Update AtomicStore.php

--- src/AtomicStore.php
+++ src/AtomicStore.php
@@ -15,6 +15,7 @@ use Sabatier\Foundation\Predicates\PredicateOperatorType;
 use Sabatier\Foundation\Set;
 use Sabatier\Foundation\URL;
 use function Sabatier\Foundation\fatal_error;
+use function Sabatier\Foundation\get_calling_class;
 use function Sabatier\Foundation\human_readable_value;
 use function Sabatier\Foundation\request_concrete_implementation;
 use const Sabatier\Foundation\NotFound;
@@ -111,6 +112,7 @@ abstract class AtomicStore extends PersistentStore
 
     private function executeFetchRequest(FetchRequest $request, ManagedObjectContext $context): ArrayClass
     {
+        error_log(__METHOD__ . " " . get_calling_class());
         $resultType = $request->resultType;
         /** @var ArrayClass<PropertyDescription|string> $propertiesToGroupBy */
         $propertiesToGroupBy = $request->propertiesToGroupBy ?? new ArrayClass();
@@ -122,7 +124,9 @@ abstract class AtomicStore extends PersistentStore
         /** @var AtomicStoreCacheNode $cacheNode */
         foreach ($this->nodeCache as $cacheNode) {
             $object = $context->object($cacheNode->objectID);
-            $this->updateObject($object);
+            if ($object->isAwakening) {
+                $this->updateObject($object);
+            }
             if ($request->includesSubentities) {
                 if ($cacheNode->objectID->entity->isKindOf($request->entity)) {
                     $objects[] = $object;
