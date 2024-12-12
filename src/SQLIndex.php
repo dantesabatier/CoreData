1879784e6174860b64a07d88a33444689f0db9ee
dantesabatier <dantesabatier@me.com>
2024-12-12 10:26:59 -0600

    Update SQLIndex.php

--- src/SQLIndex.php
+++ src/SQLIndex.php
@@ -10,29 +10,35 @@ use Sabatier\Foundation\ObjectClass;
 class SQLIndex extends ObjectClass
 {
     /** @var ArrayClass<SQLStatement> */
-    public readonly ArrayClass $createTableStatements;
+    private(set) ArrayClass $createTableStatements {
+        get {
+            if (!isset($this->createTableStatements)) {
+                $createTableStatements = new ArrayClass();
+                $elements = $this->indexDescription->elements->map(fn(FetchIndexElementDescription $element): string => "`{$element->property->name}` $element->order");
+                if ($this->isUnique) {
+                    $createTableStatements[] = new SQLStatement("ALTER TABLE `{$this->entity->tableName}` ADD CONSTRAINT `{$this->indexDescription->name}` UNIQUE INDEX IF NOT EXISTS ({$elements->join(", ")}) USING BTREE");
+                } else {
+                    $createTableStatements[] = new SQLStatement("ALTER TABLE `{$this->entity->tableName}` ADD INDEX IF NOT EXISTS `{$this->indexDescription->name}` ({$elements->join(", ")}) USING BTREE");
+                }
+                $this->createTableStatements = $createTableStatements;
+            }
+            return $this->createTableStatements;
+        }
+    }
     /** @var ArrayClass<SQLStatement> */
-    public readonly ArrayClass $dropTableStatements;
+    private(set) ArrayClass $dropTableStatements {
+        get => $this->dropTableStatements ??= new ArrayClass([new SQLStatement("ALTER TABLE `{$this->entity->tableName}` DROP INDEX IF EXISTS `{$this->indexDescription->name}`")]);
+    }
     /** @var ArrayClass<SQLStatement> */
-    public readonly ArrayClass $updateTableStatements;
+    private(set) ArrayClass $updateTableStatements {
+        get => $this->updateTableStatements ??= new ArrayClass([...$this->createTableStatements->array, ...$this->updateTableStatements->array]);
+    }
     public bool $isUnique {
         get => $this->indexDescription->isUnique;
     }
 
     public function __construct(public readonly FetchIndexDescription $indexDescription, public readonly SQLEntity $entity)
     {
-        $this->createTableStatements = new ArrayClass();
-        $this->dropTableStatements = new ArrayClass();
-        $this->updateTableStatements = new ArrayClass();
-        $this->dropTableStatements->append(new SQLStatement("ALTER TABLE `{$this->entity->tableName}` DROP INDEX IF EXISTS `{$this->indexDescription->name}`"));
-        $elements = $this->indexDescription->elements->map(fn(FetchIndexElementDescription $element): string => "`{$element->property->name}` $element->order");
-        if ($this->isUnique) {
-            $this->createTableStatements->append(new SQLStatement("ALTER TABLE `{$this->entity->tableName}` ADD CONSTRAINT `{$this->indexDescription->name}` UNIQUE INDEX IF NOT EXISTS ({$elements->join(", ")}) USING BTREE"));
-        } else {
-            $this->createTableStatements->append(new SQLStatement("ALTER TABLE `{$this->entity->tableName}` ADD INDEX IF NOT EXISTS `{$this->indexDescription->name}` ({$elements->join(", ")}) USING BTREE"));
-        }
-        $this->updateTableStatements->appendContentsOf($this->createTableStatements);
-        $this->updateTableStatements->appendContentsOf($this->dropTableStatements);
     }
 
     #[Override]
