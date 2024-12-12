900f479cea834b5d25683636ff30a821302c67e7
dantesabatier <dantesabatier@me.com>
2024-12-12 13:26:13 -0600

    Update SQLBinaryIndex.php

--- src/SQLBinaryIndex.php
+++ src/SQLBinaryIndex.php
@@ -10,8 +10,6 @@ class SQLBinaryIndex extends SQLIndex
         parent::__construct($indexDescription, $entity);
         /** @var SQLEntity $entity */
         $entity = $entity->isRootEntity ? $entity : $entity->rootEntity;
-        $this->createTableStatements->append(new SQLStatement("ALTER TABLE `$entity->tableName` ADD CONSTRAINT `$indexDescription->name` INDEX IF NOT EXISTS ({$indexDescription->elements->map(fn(FetchIndexElementDescription $element): string => "`{$element->property->name}`")->join(", ")}) USING HASH"));
-        $this->updateTableStatements->appendContentsOf($this->createTableStatements);
-        $this->updateTableStatements->appendContentsOf($this->dropTableStatements);
+        $this->createTableStatements[] = new SQLStatement("ALTER TABLE `$entity->tableName` ADD CONSTRAINT `$indexDescription->name` INDEX IF NOT EXISTS ({$indexDescription->elements->map(fn(FetchIndexElementDescription $element): string => "`{$element->property->name}`")->join(", ")}) USING HASH");
     }
 }
