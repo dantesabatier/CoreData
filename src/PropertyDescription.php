286aaeb3510b84f6df93a3aa27f6cb66c03a2f20
dantesabatier <dantesabatier@me.com>
2024-12-14 07:59:33 -0600

    Update PropertyDescription.php

--- src/PropertyDescription.php
+++ src/PropertyDescription.php
@@ -83,7 +83,7 @@ abstract class PropertyDescription extends ObjectClass
     /** @internal */
     public ?string $regex = null;
     public string $description {
-        get => sprintf("(<%s: %s>), name %s, isOptional %s, isTransient %s, entity %s renamingIdentifier %s, validation predicates %s, warnings %s", get_class($this), $this->hash, $this->name, human_readable_value($this->isOptional), human_readable_value($this->isTransient), $this->entity->name, $this->renamingIdentifier, $this->validationPredicates->description, $this->validationWarnings->description);
+        get => sprintf("(<%s: %s>), name %s, isOptional %s, isTransient %s, entity %s renamingIdentifier %s, validation predicates %s, warnings %s", $this->class, $this->hash, $this->name, human_readable_value($this->isOptional), human_readable_value($this->isTransient), $this->entity->name, $this->renamingIdentifier, $this->validationPredicates->description, $this->validationWarnings->description);
     }
 
     private function throwIfNotEditable(): void
