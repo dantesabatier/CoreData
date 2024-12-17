9a0792460c738b18c3307139bc39d7fa719dcc1c
dantesabatier <dantesabatier@me.com>
2024-12-17 10:30:35 -0600

    Update SQLFetchRequestContext.php

--- src/SQLFetchRequestContext.php
+++ src/SQLFetchRequestContext.php
@@ -88,7 +88,7 @@ class SQLFetchRequestContext extends SQLStoreRequestContext
                             $parentEntityName = $data[$currentEntity->entityKey->columnName] ?? null;
                             foreach ($keys as $key) {
                                 $property = $currentEntity->propertiesByName[$key] ?? $currentEntity->compositeAttributeNameToSQLProperty[$key];
-                                $propertyDescription = $property?->propertyDescription;
+                                $propertyDescription = $property?->propertyDescription ?? $this->request->propertiesToFetch?->first(fn(string|PropertyDescription $property): bool => $property instanceof PropertyDescription ? $property->name === $key : $property === $key);
                                 if ($property instanceof SQLRelationship || ($property instanceof SQLAttribute && $property->isCompositeAttribute)) {
                                     if ($current instanceof ArrayClass && !$current->isEmpty) {
                                         $parent = $current->first(fn(Dictionary $dictionary): bool => $dictionary[$currentEntity->primaryKey->columnName] === $parentID && $dictionary[$currentEntity->entityKey->columnName] === $parentEntityName) ?? $current[$current->indexBefore($current->endIndex)];
