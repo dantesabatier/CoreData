211feaa28523e27b15899696929a8efbab304b81
dantesabatier <dantesabatier@me.com>
2024-12-20 12:11:32 -0600

    Update ManagedObjectContext.php

--- src/ManagedObjectContext.php
+++ src/ManagedObjectContext.php
@@ -532,7 +532,7 @@ class ManagedObjectContext extends ObjectClass
     private function doPreSaveConstraintChecksForObject(ManagedObject $object): void
     {
         $committedValues = $object->committedValuesForKeys(null);
-        assert(!$committedValues->isEmpty, "Attempting to save an object with no changes $object");
+        !$committedValues->isEmpty ?: fatal_error("Attempting to save an object with no changes $object");
         /** @psalm-suppress InvalidArgument */
         $this->mergePolicy->resolveConstraintConflicts($committedValues->compactMap(function (mixed $value, string $key) use ($object): ?ConstraintConflict {
             if ($object->entity->indexes->contains(fn(FetchIndexDescription $index): bool => $index->name === $key && $index->isUnique)) {
