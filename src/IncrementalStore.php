6bc1bb8b16fc005602abb79823f58c9e7f604f97
dantesabatier <dantesabatier@me.com>
2024-12-22 05:19:49 -0600

    Update IncrementalStore.php

--- src/IncrementalStore.php
+++ src/IncrementalStore.php
@@ -10,7 +10,6 @@
 namespace Sabatier\CoreData;
 
 use Override;
-use Sabatier\Foundation\ArrayClass;
 use Sabatier\Foundation\URL;
 
 /**
@@ -19,28 +18,7 @@ use Sabatier\Foundation\URL;
 abstract class IncrementalStore extends PersistentStore
 {
     #[Override]
-    public function newValuesForObjectWithID(ManagedObjectID $objectID, ManagedObjectContext $context): ?IncrementalStoreNode
-    {
-        return null;
-    }
-
-    /**
-     * Returns a new object ID that uses given data as the key.
-     * You should not override this method.
-     * @param EntityDescription $entity The entity for the new object ID.
-     * @param int|string $referenceObject An object of type string or int to use as the key.
-     * @return ManagedObjectID A new object ID for an instance of the entity specified by entity and that uses data as the key.
-     */
-    final public function newObjectID(EntityDescription $entity, int|string $referenceObject): ManagedObjectID
-    {
-        return parent::objectID($entity, $referenceObject);
-    }
-
-    #[Override]
-    public function obtainPermanentIDs(ArrayClass $objects): ArrayClass
-    {
-        return $objects->map(fn(ManagedObject $object): ManagedObjectID => $object->objectID->isTemporaryID ? $this->objectID($object->entity, $this->newReferenceObject($object)) : $object->objectID);
-    }
+    public abstract function newValuesForObjectWithID(ManagedObjectID $objectID, ManagedObjectContext $context): ?IncrementalStoreNode;
 
     /**
      * Returns the identifier for the store at a given URL.
