c60d090469cf910e8d73d0b68e3d99cabec9137e
dantesabatier <dantesabatier@me.com>
2024-12-24 22:01:47 -0600

    Update SQLFetchRequestContext.php

--- src/SQLFetchRequestContext.php
+++ src/SQLFetchRequestContext.php
@@ -88,7 +88,7 @@ class SQLFetchRequestContext extends SQLStoreRequestContext
                                 $propertyDescription = $property?->propertyDescription ?? $this->request->propertiesToFetch?->first(fn(string|PropertyDescription $property): bool => $property instanceof PropertyDescription ? $property->name === $key : $property === $key);
                                 if ($property instanceof SQLRelationship || ($property instanceof SQLAttribute && $property->isCompositeAttribute)) {
                                     if ($current instanceof ArrayClass && !$current->isEmpty) {
-                                        $parent = $current->first(fn(Dictionary $dictionary): bool => $dictionary[$currentEntity->primaryKey->columnName] === $parentID && $dictionary[$currentEntity->entityKey->columnName] === $parentEntityName) ?? $current[$current->indexBefore($current->endIndex)];
+                                        $parent = $current->first(fn(Dictionary $dictionary): bool => $dictionary[$currentEntity->primaryKey->columnName] === $parentID && $dictionary[$currentEntity->entityKey->columnName] === $parentEntityName) ?? $current->last;
                                         /** @psalm-suppress UnsupportedReferenceUsage */
                                         $current = &$parent;
                                     }
@@ -115,7 +115,7 @@ class SQLFetchRequestContext extends SQLStoreRequestContext
                                             $current[] = new Dictionary([$currentEntity->primaryKey->columnName => $value, $currentEntity->entityKey->columnName => $currentEntity->entityDescription->name]);
                                         }
                                         if (!$current->isEmpty) {
-                                            $parent = $current->first(fn(Dictionary $dictionary): bool => $dictionary[$currentEntity->primaryKey->columnName] === $parentID && $dictionary[$currentEntity->entityKey->columnName] === $parentEntityName) ?? $current[$current->indexBefore($current->endIndex)];
+                                            $parent = $current->first(fn(Dictionary $dictionary): bool => $dictionary[$currentEntity->primaryKey->columnName] === $parentID && $dictionary[$currentEntity->entityKey->columnName] === $parentEntityName) ?? $current->last;
                                             /** @psalm-suppress UnsupportedReferenceUsage */
                                             $current = &$parent;
                                         }
