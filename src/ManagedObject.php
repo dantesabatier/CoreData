7df8c1cd47e824dcc759a5ab9eca992be6b94275
dantesabatier <dantesabatier@me.com>
2025-01-17 15:34:52 -0600

    Update ManagedObject.php

--- src/ManagedObject.php
+++ src/ManagedObject.php
@@ -135,10 +135,6 @@ class ManagedObject extends ObjectClass implements FetchRequestResult
     private(set) ArrayClass $persistentProperties {
         get => $this->persistentProperties ??= $this->modeledProperties->filter(fn(PropertyDescription $property): bool => !$property->isTransient && !$property instanceof DerivedAttributeDescription && !$property instanceof FetchedPropertyDescription);
     }
-    /** @var ArrayClass<string> */
-    private ArrayClass $attributesKeys {
-        get => $this->attributesKeys ??= $this->persistentProperties->filter(fn(PropertyDescription $property): bool => $property instanceof AttributeDescription)->map(fn(AttributeDescription $attribute): string => $attribute->name);
-    }
     /**
      * @var ArrayClass<PropertyDescription>
      * @internal
@@ -157,7 +153,7 @@ class ManagedObject extends ObjectClass implements FetchRequestResult
         get => $this->entityName ??= $this->entity->name;
     }
     public string $description {
-        get => sprintf("<%s %s> (entity: %s; id: %s %s; data: %s)", $this->class, $this->hash, $this->entity->name, $this->objectID->hash, $this->objectID->description, $this->isFault ? "<fault>" : $this->dictionaryWithValues($this->entity->propertiesByName->filter(fn(PropertyDescription $property): bool => !$this->isRelationshipForKeyFault($property->name))->keys)->description);
+        get => sprintf("<%s %s> (entity: %s; id: %s %s; data: %s)", $this->class, $this->hash, $this->entity->name, $this->objectID->hash, $this->objectID->description, $this->isFault ? "<fault>" : $this->dictionaryWithValues($this->entity->propertiesByName->filter(fn(PropertyDescription $property): bool => $this->serializationKeys->contains(fn(string $key): bool => $key === $property->name) && !$this->isRelationshipForKeyFault($property->name))->keys)->description);
     }
 
     /**
@@ -586,7 +582,7 @@ class ManagedObject extends ObjectClass implements FetchRequestResult
                     /** @var ManagedObject $managedObject */
                     foreach ($change as $managedObject) {
                         if ($member = $value->member($managedObject)) {
-                            $managedObject->setValuesForKeys($member->dictionaryWithValues($member->attributesKeys));
+                            $managedObject->setValuesForKeys($member->dictionaryWithValues($member->serializationKeys));
                         }
                     }
                 }
