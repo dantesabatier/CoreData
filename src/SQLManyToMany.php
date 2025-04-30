98b230419a6444e7de7ce5a72b3d416ff6268fb0
dantesabatier <dantesabatier@me.com>
2025-04-30 16:03:55 -0600

    Update SQLManyToMany.php

--- src/SQLManyToMany.php
+++ src/SQLManyToMany.php
@@ -24,8 +24,12 @@ class SQLManyToMany extends SQLRelationship
             return $inverseManyToMany;
         }
     }
+    /** @var ArrayClass<SQLEntity> */
+    private(set) ArrayClass $entities {
+        get => $this->entities ??= new ArrayClass([$this->destinationEntity, $this->inverseRelationship->destinationEntity])->sorted([new SortDescriptor("tableName")]);
+    }
     private(set) string $correlationTableName {
-        get => $this->correlationTableName ??= new ArrayClass([$this->destinationEntity, $this->inverseRelationship->destinationEntity])->sorted([new SortDescriptor("tableName")])->valueForKey("tableName")->join("");
+        get => $this->correlationTableName ??= $this->entities->map(fn(SQLEntity $entity): string => $entity->tableName)->join("");
     }
     private(set) string $columnName {
         get => $this->columnName ??= "{$this->name}ID";
@@ -33,15 +37,19 @@ class SQLManyToMany extends SQLRelationship
     public SQLType $columnSQLType {
         get => SQLType::int;
     }
+    /** @var ArrayClass<string> */
+    private(set) ArrayClass $columnNames {
+        get => $this->columnNames ??= new ArrayClass([$this->columnName, $this->inverseColumnName])->sort(fn(string $e, string $e1): int => ComparisonResult::orderedAscending->value * ($e <=> $e1));
+    }
     private(set) string $orderColumnName {
-        get => $this->orderColumnName ??= $this->isReflexive ? $this->columnName : new ArrayClass([$this->columnName, $this->inverseColumnName])->sort(fn(string $e, string $e1): int => ComparisonResult::orderedAscending->value * ($e <=> $e1))[0];
+        get => $this->orderColumnName ??= $this->isReflexive ? $this->columnName : $this->columnNames[0];
     }
     private(set) SQLType $orderColumnSQLType = SQLType::int;
     public string $inverseColumnName {
         get => $this->inverseManyToMany->columnName;
     }
     private(set) string $inverseOrderColumnName {
-        get => $this->inverseOrderColumnName ??= $this->isReflexive ? $this->columnName : new ArrayClass([$this->columnName, $this->inverseColumnName])->sort(fn(string $e, string $e1): int => ComparisonResult::orderedAscending->value * ($e <=> $e1))[1];
+        get => $this->inverseOrderColumnName ??= $this->isReflexive ? $this->columnName : $this->columnNames[1];
     }
     private(set) bool $isReflexive {
         get => $this->isReflexive ??= $this->columnName === $this->inverseColumnName;
