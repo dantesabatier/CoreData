1b717fe83adbe7f0c5cfdbcb1f380de2e46593e2
dantesabatier <dantesabatier@me.com>
2025-05-20 08:21:13 -0600

    Update SQLGenerator.php

--- src/SQLGenerator.php
+++ src/SQLGenerator.php
@@ -172,7 +172,6 @@ class SQLGenerator extends ObjectClass
         if ($statements->isEmpty) {
             return null;
         }
-        /** @psalm-suppress RedundantCondition, TypeDoesNotContainType */
         if (SS_COREDATA_DISABLE_FOREIGN_KEY_CHECKS) :
             if ($statements->contains(fn(SQLStatement $statement): bool => str_starts_with($statement->string, "INSERT"))) {
                 $statements->insertAt(new SQLStatement("/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */"), 0);
