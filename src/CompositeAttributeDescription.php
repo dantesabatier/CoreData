855a2e27339b817ec7c2f981d8c9c69a91d5b19b
dantesabatier <dantesabatier@me.com>
2025-06-16 07:24:24 -0600

    Update CompositeAttributeDescription.php

--- src/CompositeAttributeDescription.php
+++ src/CompositeAttributeDescription.php
@@ -13,7 +13,7 @@ use Sabatier\Foundation\KeyedUnarchiver;
  * A description of an attribute that derives its value by composing other attributes.
  *
  * Composite attributes enable you to define and store complex data types, and then query, index, and apply constraints to those types. Model classes use dictionaries to represent those composites in-memory, where each dictionary contains keys corresponding to the names of the underlying attributes. You may use composite attributes anywhere you use standard attributes. You can even nest composites inside other composites to create complex object hierarchies without additional model classes.
- * Composite attributes are available only to persistent stores that you configure with the sql store type.
+ * Composite attributes are available only to persistent stores that you configure with the SQL store type.
  */
 class CompositeAttributeDescription extends AttributeDescription
 {
