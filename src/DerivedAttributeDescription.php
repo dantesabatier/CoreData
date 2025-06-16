01db6036aef105c25706e9bd8d70d3f1f8855f10
dantesabatier <dantesabatier@me.com>
2025-06-16 07:24:27 -0600

    Update DerivedAttributeDescription.php

--- src/DerivedAttributeDescription.php
+++ src/DerivedAttributeDescription.php
@@ -16,9 +16,9 @@ use Sabatier\Foundation\Predicates\Expression;
 /**
  * A description of an attribute of a Core Data entity that derives its value from one or more other properties.
  *
- * Use derived attributes to optimize fetch performance; for example:
- * Create a derived searchName attribute to reflect a name attribute with case and diacritics removed for more efficient comparison.
- * Create a derived relationshipCount attribute to reflect the number of objects in a relationship and avoid having to do a join.
+ * Use derived attributes to optimize fetch performance; for example,
+ * Create a derived $searchName attribute to reflect a name attribute with the case and diacritics removed for more efficient comparison.
+ * Create a derived $relationshipCount attribute to reflect the number of objects in a relationship and avoid having to do a join.
  */
 class DerivedAttributeDescription extends AttributeDescription
 {
