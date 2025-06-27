625baeb36b24221e873b3cd04de59826e6678600
dantesabatier <dantesabatier@me.com>
2025-06-27 11:18:18 -0600

    Update FaultingSet.php

--- src/FaultingSet.php
+++ src/FaultingSet.php
@@ -27,8 +27,8 @@ class FaultingSet extends Set
     #[Override]
     public function indexOf(mixed $element): ?int
     {
-        if ($element instanceof ManagedObject) {
-            $element = $element->objectID;
+        if ($element instanceof ManagedObjectID) {
+            $element = $this->source->managedObjectContext->object($element);
         }
         return parent::indexOf($element);
     }
