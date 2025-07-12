33e33c580327791646be7f0e0aec591e83559aab
dantesabatier <dantesabatier@me.com>
2025-07-12 10:15:00 -0600

    Update ManagedObject.php

--- src/ManagedObject.php
+++ src/ManagedObject.php
@@ -242,7 +242,7 @@ class ManagedObject extends ObjectClass implements FetchRequestResult
             $value = $this->primitiveValueForKey($key);
             if ($property instanceof AttributeDescription && !$property instanceof DerivedAttributeDescription) {
                 $value ??= $property->defaultValue;
-                if ($value === null && !$property->isOptional) {
+                if ($value === null && !$property->isOptional && $this->objectID->isTemporaryID) {
                     $value = match ($property->type) {
                         AttributeType::integer16, AttributeType::integer32, AttributeType::integer64, AttributeType::decimal, AttributeType::double, AttributeType::float, AttributeType::string, AttributeType::boolean => self::coercedValue($value, $property->type, $property->attributeValueClassName, $property->valueTransformerName, $property->isOptional),
                         default => null
