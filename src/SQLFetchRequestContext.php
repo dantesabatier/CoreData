a6431da038ddb1957748173b6539320135abb687
dantesabatier <dantesabatier@me.com>
2025-07-26 04:03:20 -0600

    Update SQLFetchRequestContext.php

--- src/SQLFetchRequestContext.php
+++ src/SQLFetchRequestContext.php
@@ -113,8 +113,7 @@ class SQLFetchRequestContext extends SQLStoreRequestContext
                         if ($property instanceof SQLColumn || $propertyDescription instanceof ExpressionDescription) {
                             if ($current instanceof ArrayClass) {
                                 if ($property instanceof SQLPrimaryKey && !$current->contains(fn(Dictionary $dictionary): bool => $dictionary[$currentEntity->primaryKey->columnName] === $currentID && $dictionary[$currentEntity->entityKey->columnName] === $currentEntity->entityDescription->name)) {
-                                    $element = new Dictionary([$currentEntity->primaryKey->columnName => $currentID, $currentEntity->entityKey->columnName => $currentEntity->entityDescription->name]);
-                                    $current->append($element);
+                                    $current->append(new Dictionary([$currentEntity->primaryKey->columnName => $currentID, $currentEntity->entityKey->columnName => $currentEntity->entityDescription->name]));
                                 }
                                 if (!$current->isEmpty) {
                                     $element = $current->first(fn(Dictionary $dictionary): bool => $dictionary[$currentEntity->primaryKey->columnName] === $currentID && $dictionary[$currentEntity->entityKey->columnName] === $currentEntity->entityDescription->name) ?? $current->last;
