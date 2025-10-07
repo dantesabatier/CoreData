81ee80867b1b31f94bd2e4573559126489f280e4
dantesabatier <dantesabatier@me.com>
2025-10-07 09:48:25 -0600

    Update MappingModelBuilder.php

--- src/MappingModelBuilder.php
+++ src/MappingModelBuilder.php
@@ -25,7 +25,7 @@ class MappingModelBuilder
     public function canTransformAttributeType(AttributeType $source, AttributeType $destination): bool
     {
         return match ($source) {
-            AttributeType::integer16, AttributeType::integer32, AttributeType::integer64, AttributeType::decimal, AttributeType::double, AttributeType::float, AttributeType::boolean => $destination === AttributeType::integer16 || $destination === AttributeType::integer32 || $destination === AttributeType::integer64 || $destination === AttributeType::decimal || $destination === AttributeType::double || $destination === AttributeType::float || $destination === AttributeType::string,
+            AttributeType::integer16, AttributeType::integer32, AttributeType::integer64, AttributeType::decimal, AttributeType::double, AttributeType::float, AttributeType::boolean => in_array($destination, [AttributeType::integer16, AttributeType::integer32, AttributeType::integer64, AttributeType::decimal, AttributeType::double, AttributeType::float, AttributeType::string], true),
             default => $source === $destination,
         };
     }
