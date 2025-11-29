c1c8977baf6aeb441155c5a4e054d83a7ebe4619
dantesabatier <dantesabatier@me.com>
2025-11-28 22:35:15 -0600

    Fix spacing inconsistency in string encoding check within `SQLStatementFormatter`

--- src/SQLStatementFormatter.php
+++ src/SQLStatementFormatter.php
@@ -30,7 +30,7 @@ class SQLStatementFormatter extends Formatter
             $string = sprintf(str_replace(["%", "?"], ["%%", "%s"], $string), ...$object->arguments->map(fn(mixed $e): string => match (typeof($e)) {
                 ManagedObjectID::class => (string)$e->referenceObject,
                 Date::class, UUID::class, URL::class => "'$e'",
-                "string" => mb_check_encoding($e, "UTF-8") ? "'" . addslashes((string) $e) . "'" : "(binary data)",
+                "string" => mb_check_encoding($e, "UTF-8") ? "'" . addslashes((string)$e) . "'" : "(binary data)",
                 default => (function () use ($e): string {
                     if ($e instanceof Value) {
                         $e = $e->value;
