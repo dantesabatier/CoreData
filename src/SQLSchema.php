6cf95db2f57688db3b2fc937fc97eb3d7ce88194
dantesabatier <dantesabatier@me.com>
2025-12-18 15:56:57 -0600

    Refactor SQLSchema for improved type safety and final class declaration
    Made `SQLSchema` a `final` class, replaced `SQLSchema` with `self` in static factory method return type, and adjusted constructor logic for consistency and clarity.

--- src/SQLSchema.php
+++ src/SQLSchema.php
@@ -13,14 +13,14 @@ use Sabatier\Foundation\ProcessInfo;
 use function Sabatier\Foundation\fatal_error;
 
 /** @internal */
-class SQLSchema
+final class SQLSchema
 {
-    public function __construct(public string $name, public string $host, public SQLCredential $credential, public string $engine = "InnoDB", public string $charset = "utf8",  public string $collation = "utf8_general_ci", public string $socket = "/tmp/mysql.sock")
+    public function __construct(public string $name, public string $host, public SQLCredential $credential, public string $engine = "InnoDB", public string $charset = "utf8", public string $collation = "utf8_general_ci", public string $socket = "/tmp/mysql.sock")
     {
     }
 
-    public static function schema(?string $name = null): SQLSchema
+    public static function schema(?string $name = null): self
     {
-        return new SQLSchema($name ?? ProcessInfo::processInfo()->environment["SQL_SCHEMA_NAME"] ?? fatal_error("Environment variable \"SQL_SCHEMA_NAME\" cannot be null"), ProcessInfo::processInfo()->environment["SQL_SCHEMA_HOST"] ?? "localhost", new SQLCredential(ProcessInfo::processInfo()->environment["SQL_SCHEMA_CREDENTIAL_USER"] ?? "root", ProcessInfo::processInfo()->environment["SQL_SCHEMA_CREDENTIAL_PASSWORD"]));
+        return new self($name ?? ProcessInfo::processInfo()->environment["SQL_SCHEMA_NAME"] ?? fatal_error("Environment variable \"SQL_SCHEMA_NAME\" cannot be null"), ProcessInfo::processInfo()->environment["SQL_SCHEMA_HOST"] ?? "localhost", new SQLCredential(ProcessInfo::processInfo()->environment["SQL_SCHEMA_CREDENTIAL_USER"] ?? "root", ProcessInfo::processInfo()->environment["SQL_SCHEMA_CREDENTIAL_PASSWORD"]));
     }
 }
