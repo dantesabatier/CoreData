aa80ed8c9b9b12dcf5d51abecf4d29c7768df3c7
dantesabatier <dantesabatier@me.com>
2025-12-27 14:50:30 -0600

    Update FetchedResultsControllerDelegate type hints and docs
    Refined PHPDoc comments for clarity and corrected parameter types in controllerDidChangeObject to allow nullable IndexPath values. Minor documentation improvements for consistency and accuracy.

--- src/FetchedResultsControllerDelegate.php
+++ src/FetchedResultsControllerDelegate.php
@@ -11,7 +11,7 @@ use Sabatier\Foundation\IndexPath;
 interface FetchedResultsControllerDelegate
 {
     /**
-     * Notifies the receiver about changes to the content in the fetched results controller, by using a collection difference.
+     * Notifies the receiver about changes to the content in the fetched results controller by using a collection difference.
      *
      * To apply the changes, call {@see applySnapshotAnimatingDifferences()} on the collection or table view’s data source.
      * If this method is implemented, no other delegate methods are invoked.
@@ -21,7 +21,7 @@ interface FetchedResultsControllerDelegate
     public function controllerDidChangeContentWithSnapshot(FetchedResultsController $controller, mixed $snapshot): void;
 
     /**
-     * Notifies the receiver about changes to the content in the fetched results controller, by using a collection difference.
+     * Notifies the receiver about changes to the content in the fetched results controller by using a collection difference.
      *
      * This method is only invoked if the controller’s {@see FetchedResultsController::$sectionNameKeyPath} property is null and {@see controllerDidChangeContentWithSnapshot()} is not implemented.
      * If this method is implemented, no other delegate methods are invoked.
@@ -51,11 +51,11 @@ interface FetchedResultsControllerDelegate
      * This method may be invoked many times during an update event (for example, if you are importing data on a background thread and adding them to the context in a batch). You should consider carefully whether you want to update the table view on receipt of each message.
      * @param FetchedResultsController $controller The fetched results controller that sent the message.
      * @param mixed $object The object in controller’s fetched results that changed.
-     * @param IndexPath $indexPath The index path of the changed object (this value is null for insertions).
+     * @param IndexPath|null $indexPath The index path of the changed object (this value is null for insertions).
      * @param FetchedResultsChangeType $type The type of change.
-     * @param IndexPath $newIndexPath The destination path for the object for insertions or moves (this value is null for a deletion).
+     * @param IndexPath|null $newIndexPath The destination path for the object for insertions or moves (this value is null for a deletion).
      */
-    public function controllerDidChangeObject(FetchedResultsController $controller, mixed $object, IndexPath $indexPath, FetchedResultsChangeType $type, IndexPath $newIndexPath): void;
+    public function controllerDidChangeObject(FetchedResultsController $controller, mixed $object, ?IndexPath $indexPath, FetchedResultsChangeType $type, ?IndexPath $newIndexPath): void;
 
     /**
      * Notifies the receiver of the addition or removal of a section.
@@ -80,8 +80,8 @@ interface FetchedResultsControllerDelegate
     /**
      * Returns the name for a given section.
      *
-     * This method does not enable change tracking. It is only needed if a section index is used.
-     * If the delegate doesn't implement this method, the default implementation returns the capitalized first letter of the section name ({@see FetchedResultsController::sectionIndexTitle()}  in FetchedResultsController).
+     * This method does not enable change tracking. It is only necessary if a section index is used.
+     * If the delegate doesn't implement this method, the default implementation returns the capitalized first letter of the section name ({@see FetchedResultsController::sectionIndexTitle()} in FetchedResultsController).
      * @param FetchedResultsController $controller The fetched results controller that sent the message.
      * @param string $sectionName The default name of the section.
      * @return string The string to use as the name for the specified section.
