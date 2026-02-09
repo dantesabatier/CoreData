f76324f5cc86bc5dd8734809accc0ba2f52bcd1c
dantesabatier <dantesabatier@me.com>
2026-02-09 11:23:57 -0600

    Enhance originalSnapshot setter in ManagedObject for improved filtering and default value assignment
    - Update the setter logic to filter the originalSnapshot values more clearly and assign default values for entityName and version if not present.
    - Ensure that the setter handles null values appropriately, maintaining the integrity of the ManagedObject state.

--- src/ManagedObject.php
+++ src/ManagedObject.php
@@ -190,10 +190,15 @@ class ManagedObject extends ObjectClass implements FetchRequestResult
      */
     public ?Dictionary $originalSnapshot = null {
         set {
-            $this->originalSnapshot = $value?->filter(fn(mixed $value, string $key): bool => match ($key) {
-                "isInserted", "isFault", "faultingState" => false,
-                default => true,
-            });
+            if ($value) {
+                $value = $value->filter(fn(mixed $value, string $key): bool => match ($key) {
+                    "isInserted", "isFault", "faultingState" => false,
+                    default => true,
+                });
+                $value[ManagedObjectEntityNameKey] ??= $this->entityName;
+                $value[ManagedObjectVersionKey] ??= $this->version;
+            }
+            $this->originalSnapshot = $value;
         }
     }
     /**
