ee9e2c1e27b167035065b3675d5b99b439ed3a2a
dantesabatier <dantesabatier@me.com>
2026-04-14 14:37:23 -0600

    Update QueryGenerationToken.php

--- src/QueryGenerationToken.php
+++ src/QueryGenerationToken.php
@@ -2,6 +2,11 @@
 
 namespace Sabatier\CoreData;
 
+/**
+ * A token that indicates which generation of the persistent store is being accessed.
+ *
+ * When a managed object context is pinned to a specific generation of the app data, a query generation token will be associated with that context.
+ */
 final readonly class QueryGenerationToken
 {
     public function __construct(public string $storeIdentifier, public int $origin, public int $generation)
