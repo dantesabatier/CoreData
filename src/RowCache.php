c685dece254de044d2de0a3a0f2256369c802596
dantesabatier <dantesabatier@me.com>
2026-04-19 21:39:08 -0600

    Update RowCache.php

--- src/RowCache.php
+++ src/RowCache.php
@@ -19,13 +19,13 @@ abstract class RowCache implements PersistentStoreCache
     {
         $name = $property?->name ?? "";
         $divider = $property ? "/" : "";
-        return "{$objectID->uriRepresentation()->absoluteString}$divider$name";
+        return md5("{$objectID->uriRepresentation()->absoluteString}$divider$name");
     }
 
     #[Override]
     public function queryKeyForRequest(FetchRequest $request, QueryGenerationToken $token): string
     {
-        $hash = urlencode($request->canonicalDescription);
+        $hash = md5($request->canonicalDescription);
         return "/query/$token->origin/$token->generation/$hash";
     }
 }
