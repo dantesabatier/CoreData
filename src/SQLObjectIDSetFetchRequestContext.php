5a58a968059b93456067976f31ff9c0595d80ae5
dantesabatier <dantesabatier@me.com>
2026-04-23 16:33:40 -0600

    Update SQLObjectIDSetFetchRequestContext.php

--- src/SQLObjectIDSetFetchRequestContext.php
+++ src/SQLObjectIDSetFetchRequestContext.php
@@ -4,10 +4,10 @@ namespace Sabatier\CoreData;
 
 use Override;
 use Sabatier\Foundation\ArrayClass;
-use Sabatier\Foundation\SortDescriptor;
 use Sabatier\Foundation\Predicates\ComparisonPredicate;
 use Sabatier\Foundation\Predicates\Expression;
 use Sabatier\Foundation\Predicates\PredicateOperatorType;
+use Sabatier\Foundation\SortDescriptor;
 use function Sabatier\Foundation\fatal_error;
 
 /** @internal */
@@ -18,16 +18,16 @@ final class SQLObjectIDSetFetchRequestContext extends SQLFetchRequestContext
 
     public function __construct(FetchRequest $request, ManagedObjectContext $context, SQLCore $sqlCore, ArrayClass $idSets, string $columnName)
     {
-    	/** @var string $entityName */
-    	$entityName = $request->entity?->name;
-    	/** @var SQLEntity $entity */
-    	$entity = $sqlCore->model->entitiesByName[$entityName] ?? fatal_error("Entity not found for relationship fault: $entityName");
-    	/** @var FetchRequest<ManagedObjectID> $fetchRequest */
-    	$fetchRequest = clone($request, [
-    		"predicate" => new ComparisonPredicate(Expression::expressionForKeyPath($entity->primaryKey->columnName), Expression::expressionForConstantValue($idSets), PredicateOperatorType::in),
-      		"sortDescriptors" => new ArrayClass([new SortDescriptor($columnName)]),
-        	"resultType" => FetchRequestResultType::managedObjectIDResultType,
-    	]);
+        /** @var string $entityName */
+        $entityName = $request->entity?->name;
+        /** @var SQLEntity $entity */
+        $entity = $sqlCore->model->entitiesByName[$entityName] ?? fatal_error("Entity not found for relationship fault: $entityName");
+        /** @var FetchRequest<ManagedObjectID> $fetchRequest */
+        $fetchRequest = clone($request, [
+            "predicate" => new ComparisonPredicate(Expression::expressionForKeyPath($entity->primaryKey->columnName), Expression::expressionForConstantValue($idSets), PredicateOperatorType::in),
+            "sortDescriptors" => new ArrayClass([new SortDescriptor($columnName)]),
+            "resultType" => FetchRequestResultType::managedObjectIDResultType,
+        ]);
         parent::__construct($fetchRequest, $context, $sqlCore);
         $this->idSets = $idSets;
         $this->columnName = $columnName;
@@ -36,10 +36,10 @@ final class SQLObjectIDSetFetchRequestContext extends SQLFetchRequestContext
     #[Override]
     protected function executeRequestCore(): bool
     {
-   		$debugLevel = $this->debugLevel;
-   		$this->debugLevel = SQLDebugLevel::none;
-   		parent::executeRequestCore();
-   		$this->debugLevel = $debugLevel;
+        $debugLevel = $this->debugLevel;
+        $this->debugLevel = SQLDebugLevel::none;
+        parent::executeRequestCore();
+        $this->debugLevel = $debugLevel;
         return true;
     }
 }
